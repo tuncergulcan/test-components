f11398940639e8a7a958b673887f90b9b8965501
Tuncer Gülcan <tuncer.gulcan@tiga.com.tr>
2021-10-12 17:59:01 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import AwesomeButton from './button.js'
+import CustomTabPanel from "./CustomTabPanel.js";
 
 const returnLibrary = () => {
-    return {
-        AwesomeButton: AwesomeButton
-        // you can add here other components that you want to export
-    }
-}
-export default returnLibrary()
+  return {
+    CustomTabPanel: CustomTabPanel,
+    // you can add here other components that you want to export
+  };
+};
+export default returnLibrary();
